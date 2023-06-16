0e68b381bf73e963bbf577fc2740adcb4190ca8e
mar1fatih <marouanefatih631@gmail.com>
2023-06-15 18:54:43 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * description: a program that print the size of types
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	pintf("Size of a char: %lu byte(s)", sizeof(char));
+	pintf("Size of an int: %lu byte(s)", sizeof(int);
+	pintf("Size of a long int: %lu byte(s)", sizeof(long int);
+	pintf("Size of a long long int: %lu byte(s)", sizeof(long long int);
+	pintf("Size of a float: %lu byte(s)", sizeof(float);
+	return (0);
+}
