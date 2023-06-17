db5ffea43958f39180049d3670fcf7ec2bbfa7d7
mar1fatih <marouanefatih631@gmail.com>
2023-06-16 18:24:00 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: a program print alphabet without e and q
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+	printf("%d", num);
+	num++;
+	}
+	return (0);
+}
