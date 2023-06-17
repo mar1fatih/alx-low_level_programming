89e67338bd8d26400cdd4a3baa804efc081e89c1
mar1fatih <marouanefatih631@gmail.com>
2023-06-17 10:58:26 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: a program print alphabet without e and q
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	int f, s;
+
+	for (f = 0; f <= 9; f++)
+	{
+	for (s = f + 1; s <= 9; s++)
+	{
+	putchar(f + '0');
+	putchar(s + '0');
+	if (f != 8 || s != 9)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	return (0);
+}
