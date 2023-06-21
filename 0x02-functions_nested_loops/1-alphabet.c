9911bf17741cf18a3e619661fe2a10219f3b0a1c
mar1fatih <marouanefatih631@gmail.com>
2023-06-20 17:33:20 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - using _putchar to print a to z
+ * a - z
+*/
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+	_putchar(i);
+	}
+	_putchar('\n');
+}
