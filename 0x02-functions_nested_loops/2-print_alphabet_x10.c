932a16fe96c3ae335af833f9a84b9e4d6b3b43f7
mar1fatih <marouanefatih631@gmail.com>
2023-06-20 18:01:27 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - using _putchar to print a to z 10 times
+ *
+*/
+void print_alphabet_x10(void)
+{
+	int line, i;
+
+	for (line = 1; line <= 10; line++)
+	{
+	for (i = 'a'; i <= 'z'; i++)
+	}
+	_putchar(i);
+	_putchar('\n');
+	}
+	}
+}
