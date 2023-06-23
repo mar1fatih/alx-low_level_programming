5b4b73ae62c62e8924bac58475c7958554560fee
mar1fatih <marouanefatih631@gmail.com>
2023-06-22 18:19:38 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers - a
+*/
+void print_most_numbers(void)
+{
+int i;
+
+for (i = 0; i <= 9; i++)
+{
+if (i == 2 || i == 4)
+{}
+else
+{
+_putchar(i + 48);
+}
+}
+_putchar('\n');
+}
