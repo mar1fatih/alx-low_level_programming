4a425c34e660779805b4ea1c5677edd01b4c2062
mar1fatih <marouanefatih631@gmail.com>
2023-06-22 19:51:30 -0700

    print_square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_square - a
+ * @size: b
+*/
+void print_square(int size)
+{
+int x, y;
+for (x = 1; x <= size; x++)
+{
+for (y = 1; y <= size; y++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
