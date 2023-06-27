6628fb6b32ae0ca2e854b533f909c7d8fdd5de7f
mar1fatih <marouanefatih631@gmail.com>
2023-06-26 19:02:55 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * rev_string - s
+ * @s: b
+*/
+void rev_string(char *s)
+{
+	int len, i;
+	char c;
+
+	for (len = 0; s[len] != '\0'; ++len)
+	;
+	for (i = 0; i < len / 2; i++)
+	{
+	c = s[i];
+	s[i] = s[len - 1 - i];
+	s[len - 1 - i] = c;
+	}
+}
