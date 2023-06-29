a0458e42bcaf62d9df541dec1e4b15dbec5ea58f
mar1fatih <marouanefatih631@gmail.com>
2023-06-29 10:50:26 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * string_toupper - a
+ * @st: d
+ * Return: st
+*/
+char *string_toupper(char *st)
+{
+int i;
+
+for (i = 0; st[i] != '\0'; i++)
+{
+	if (st[i] >= 97 && st[i] <= 122)
+	{
+	st[i] = st[i] - 32;
+	}
+}
+return (st);
+}
