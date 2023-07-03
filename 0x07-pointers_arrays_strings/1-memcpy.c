8469cc94a9e0e482ae9fdf63f436b0a3b95a4fe1
mar1fatih <marouanefatih@gmail.com>
2023-07-03 09:23:21 -0700

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memcpy - a
+ * @dest: b
+ * @src: c
+ * @n: d
+ * Return: dest
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+int i = 0;
+
+while (i < n)
+{
+dest[i] = src[i];
+i++;
+}
+printf("\n");
+return (dest);
+}
