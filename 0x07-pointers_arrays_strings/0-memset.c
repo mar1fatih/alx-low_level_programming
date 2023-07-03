3c6020ac6139e30911d887553ecdaf93747ae0ea
mar1fatih <marouanefatih@gmail.com>
2023-07-03 09:00:59 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,22 +9,12 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i = 0;
+	unsigned int i = 0;
 
 	while (i < n)
 	{
-	if (i % 10)
-	{
-	printf(" ");
-	}
-	if (!(i % 10) && i)
-	{
-	printf("\n");
-	}
-
 	s[i] = b;
 	i++;
 	}
-	printf("\n");
 	return (s);
 }
