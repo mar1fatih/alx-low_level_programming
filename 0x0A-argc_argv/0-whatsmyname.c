6744c902bdbfc9ed91b24378417f8283979c06ae
mar1fatih <marouanefatih@gmail.com>
2023-07-07 18:33:02 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - a
+ * @argc: d
+ * @argv: f
+ * Return: always 0
+*/
+int main(int argc, char const **argv)
+{
+(void)argc;
+printf("%s\n", *argv);
+return (0);
+}
