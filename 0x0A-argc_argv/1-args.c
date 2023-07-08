b9e4cbe296f7a36ff7af500a9548dac7dcffa122
mar1fatih <marouanefatih@gmail.com>
2023-07-07 18:40:31 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - a
+ * @argc: d
+ * @argv: f
+ * Return: always 0
+*/
+int main(int argc, char const **argv)
+{
+(void)argv;
+printf("%d\n", argc - 1);
+return (0);
+}
