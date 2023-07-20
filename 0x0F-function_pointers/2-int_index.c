59bb5d7246694904bb9c2211a58fccb421c89767
mar1fatih <marouanefatih631@gmail.com>
2023-07-19 18:00:28 -0700

    2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+/**
+ * int_index - l
+ * @array: k
+ * @size: k
+ * @cmp: k
+ * Return: int
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+
+if (size <= 0)
+{
+return (-1);
+}
+for (i = 0; i < (int) size; i++)
+{
+if (cmp(*(array + i)) != 0)
+{
+return (i);
+}
+}
+return (-1);
+}
