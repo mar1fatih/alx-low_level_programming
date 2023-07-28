619c2cf011546dfb98cc4b62482ff1ce5c04e782
mar1fatih <marouanefatih631@gmail.com>
2023-07-28 01:08:58 +0000

    1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * list_len - l
+ * @h: l
+ * Return: struct
+*/
+size_t list_len(const list_t *h)
+{
+size_t len = 0;
+while(h != NULL)
+{
+h = h->next;
+len++;
+}
+return (len);
+}
