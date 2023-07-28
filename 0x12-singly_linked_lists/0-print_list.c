4b6801c14a92c0ba0ad0860d6257af251f5be2ac
mar1fatih <marouanefatih631@gmail.com>
2023-07-28 00:48:09 +0000

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+/**
+ * len - length of string
+ * @str: l
+ * Return: int
+ */
+int len(char *str)
+{
+int i = 0;
+if (str == NULL)
+{
+return (0);
+}
+while (*str != '\0')
+{
+str++;
+i++;
+}
+return (i);
+}
+
+/**
+ * print_list - k
+ * @h: k
+ * Return: struct
+*/
+size_t print_list(const list_t *h)
+{
+size_t j = 0;
+while (h != NULL)
+{
+printf("[%d] %s\n", len(h->str), h->str ? h->str : "(nill)");
+h = h->next;
+j++;
+}
+return (i);
+}
