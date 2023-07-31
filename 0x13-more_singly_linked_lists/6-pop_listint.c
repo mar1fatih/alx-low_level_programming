57681df691f66bc61f6a8ddef6b978c0298913ec
mar1fatih <marouanefatih631@gmail.com>
2023-07-31 20:43:22 +0000

    6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,6 +9,8 @@ int pop_listint(listint_t **head)
 	listint_t *current;
 	int n = 0;
 
+	if (head == NULL || *head == NULL)
+		return (0);
 	n = (*head)->n;
 	current = *head;
 	*head = (*head)->next;
