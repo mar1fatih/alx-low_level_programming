17f5f9d3695ac101efde161e678b95dd8c0e0372
mar1fatih <marouanefatih631@gmail.com>
2023-07-31 11:58:32 +0000

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * print_listint - check code
+ * @h: head
+ * Return: size_t
+*/
+size_t print_listint(const listint_t *h)
+{
+	size_t c;
+
+	c = 0;
+	while (h)
+	{
+		printf("%d\n", h->n);
+		c++;
+		h = h->next;
+	}
+	return (c);
+}
