1fd71b945a7b59ac1592b813c2777fb387d11f4b
mar1fatih <marouanefatih631@gmail.com>
2023-07-31 19:42:23 +0000

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_listint2 - check the code
+ * @head: head
+*/
+void free_listint2(listint_t *head)
+{
+	listint_t *current;
+	listint_t *ptr;
+
+	current = head;
+	while (current)
+	{
+		ptr = current;
+		current = current->next;
+		free(ptr);
+	}
+	*head = NULL;
+}
