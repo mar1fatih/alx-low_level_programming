ac656f889c9e1807203234e16aef24ec5891dce6
mar1fatih <marouanefatih631@gmail.com>
2023-07-31 20:33:53 +0000

    6-pop_listint.c

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * pop_listint - check code
+ * @head: head
+ * Return: n
+*/
+int pop_listint(listint_t **head)
+{
+	listint_t *current;
+	int n = 0;
+
+	n = (*head)->n;
+	current = *head;
+	*head = (*head)->next;
+	free(current);
+	return (n);
+}
