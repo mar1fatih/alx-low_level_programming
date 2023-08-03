72cfc4d348c2847d4cfdb9b0d0d67de42b7c31d6
mar1fatih <marouanefatih631@gmail.com>
2023-08-03 21:47:52 +0000

    5-flip_bits.c

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * flip_bits - check the code
+ * @n: n
+ * @m: m
+ * Return: int
+*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+unsigned long int i;
+int c;
+
+i = n ^ m;
+while (i != 0)
+{
+if ((i & 1ul) != 0)
+{
+c++;
+}
+i = i >> 1ul;
+}
+return (c);
+}
