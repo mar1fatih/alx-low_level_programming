c24c5749f41b7008562a9a39feef7827ed9cf57f
mar1fatih <marouanefatih631@gmail.com>
2023-08-08 01:18:21 +0000

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,56 @@
+#include "main.h"
+/**
+ * slen - length of string
+ * @str: l
+ * Return: int
+*/
+int slen(char *str)
+{
+int i = 0;
+
+if (str == NULL)
+{
+return (0);
+}
+while (*str++)
+{
+i++;
+}
+return (i);
+}
+
+/**
+ * create_file - check the code
+ * @filename: filename
+ * @text_content: text_content
+ * Return: int
+*/
+int create_file(const char *filename, char *text_content)
+{
+ssize_t i = 0;
+int ptr, l;
+
+l = slen(text_content);
+if (filename == NULL)
+{
+return (0);
+}
+ptr = open(filename, O_WRONLY | O_CREAT | O_TRUNC);
+if (ptr == -1)
+{
+return (0);
+}
+if (l > 0)
+{
+i = write(ptr, text_content, l);
+}
+close(ptr);
+if (i == l)
+{
+return (1);
+}
+else
+{
+return (-1);
+}
+}
