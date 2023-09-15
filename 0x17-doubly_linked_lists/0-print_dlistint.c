a7695b34c839bf65f73754e9f0b26c30f88eff1a
mar1fatih <marouanefatih631@gmail.com>
2023-09-15 00:14:45 +0000

    task0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * print_dlistint - check code
+ * @h: head
+ * Return: size_t
+*/
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t x = 0;
+	const dlistint_t *current;
+
+	current = h;
+	while(current)
+	{
+		printf("%d\n", current->n);
+		x++;
+		current = current->next;
+	}
+	return (x);
+}
