c92c688a6ff17d27adc4df4c30991f952a0fc429
mar1fatih <marouanefatih631@gmail.com>
2023-10-06 12:01:43 +0000

    task3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (!ht || !key)
 		return (0);
-	index = key_index(key, ht->size);
+	index = key_index((unsigned const char *)key, ht->size);
 	current = ht->array[index];
 	while (current)
 	{
