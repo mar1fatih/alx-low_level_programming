a766d356ad61c4ea051643d7316e54ae32830a60
mar1fatih <marouanefatih631@gmail.com>
2023-10-06 14:30:28 +0000

    task5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - check the code
+ * @ht: ht
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *current;
+	int c = 0;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		current = ht->array[i];
+		while (current)
+		{
+			if (c != 0)
+			{
+				printf(", ");
+			}
+			printf("'%s' : '%s'", current->key, current->value);
+			c++;
+			current = current->next;
+		}
+	}
+	printf("}\n");
+}
