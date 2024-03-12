eac1f38909785fac8310d77401c3cc09f733e68c
mar1fatih <marouanefatih631@gmail.com>
2024-03-12 09:10:30 -0700

    task 0

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,23 @@
+#include "search_algos.h"
+/**
+ * linear_search - serching using linear search
+ * @array: array
+ * @size: array size
+ * @value: the value to be looking for
+ * Return: the value index
+*/
+int linear_search(int *array, size_t size, int value)
+{
+	int idx = 0;
+	if (array == NULL)
+		return (-1);
+	while(size--)
+	{
+		printf("Value checked array[%d] = [%d]\n", idx, *array);
+		if (*array == value)
+			return (idx);
+		idx++;
+		array++;
+	}
+	return (-1);
+}
